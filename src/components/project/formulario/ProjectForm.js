4b5522cff563d6d11f73b2dc5ff109e2c8bb3286
Estefane <estefaneribeiroveras@gmail.com>
2022-09-17 12:24:00 -0300

    c3 - criação  e estilização de componentes e api

--- src/components/project/formulario/ProjectForm.js
+++ src/components/project/formulario/ProjectForm.js
@@ -1,24 +1,31 @@
-function ProjectForm() {
-  return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto:" />
-      </div>
+import Input from '../../form/Input'
+import Select from '../../form/Select'
+import Submit from '../../form/Submit'
+import styles from './ProjectForm.module.css'
 
-      <div>
-        <input
-          type="number"
-          placeholder="Insira o orçamento total do projeto:"
-        />
-      </div>
-      <div>
-        <select name="category_id">
-          <option disabled>Selecione a categoria</option>
-        </select>
-      </div>
-      <div>
-        <input type="submit" />
-      </div>
+function ProjectForm({ btnText }) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text={'Nome do projeto'}
+        placeholder={'Insira o nome do projeto:'}
+        name={'nomeProjeto'}
+      />
+
+      <Input
+        type="number"
+        text={'Orçamento do produto'}
+        placeholder={'Insira o orçamento total do projeto:'}
+        name={'budget'}
+      />
+
+      <Select
+        name="category_id"
+        text="Selecione uma categoria"
+        id="category_id"
+      />
+      <Submit text={btnText} />
     </form>
   )
 }
